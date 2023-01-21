253b8e2be532ab7d2e84e2a58fae8ea3452f1ead
Akki2155 <akashsangwan98@gmail.com>
2023-01-21 18:51:51 +0530

    Setting Home Route

--- index.js
+++ index.js
@@ -13,12 +13,17 @@ app.use(bodyParser.json({ limit:"30mb", extended:true }));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended:true }));
 app.use(cors());
 
+app.get("/", (req,res)=>{
+    res.send("Working good");
+})
+
 app.use('/posts', postRoutes)
 app.use('/user', userRoutes)
 
 
 const CONNECTION_URL='mongodb+srv://Akki2155:akki3009@cluster0.hjhrres.mongodb.net/?retryWrites=true&w=majority';
 const PORT= process.env.PORT || 4000;
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true }).then(()=>
     app.listen(PORT, ()=> console.log(`Server running on port : ${PORT}`))
